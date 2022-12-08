7279bfcce873063dc9a4fecbaaaf158762a345b5
yoconguyen <hoang10x77@gmail.com>
2022-12-08 09:36:36 +0700

    up

---
+++ src/main/java/com/example/demo1thayloc/Demo1ThayLocApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1thayloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1ThayLocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1ThayLocApplication.class, args);
+    }
+
+}
